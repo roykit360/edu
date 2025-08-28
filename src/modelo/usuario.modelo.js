f05cdd41478191336687b09b621a05d76ffd940c
roykit360 <rroy87872@gmail.com>
2025-08-28 13:39:05 -0500

    Update

--- src/modelo/usuario.modelo.js
+++ src/modelo/usuario.modelo.js
@@ -2,14 +2,35 @@ import PG from "pg";
 
 const { Client } = PG;
 
-const client = new Client({
-	user: 'neondb_owner',
-	host: 'ep-lucky-fog-a2x9zvmh.eu-central-1.aws.neon.tech',
-	database: 'neondb',
-	password: 'npg_2SAdCYxnMe6T',
-	port: 5432,
-	ssl: { rejectUnauthorized: false }
-});
+// Función para conectarse a la base de Datos
+export async function crearUsuario({ nombre, correo, contrasena }) {
+	const client = new Client({
+		user: 'neondb_owner',
+		host: 'ep-lucky-fog-a2x9zvmh.eu-central-1.aws.neon.tech',
+		database: 'neondb',
+		password: 'npg_2SAdCYxnMe6T',
+		port: 5432,
+		ssl: { rejectUnauthorized: false }
+	});
+
+// Función para crear un nuevo usuario
+	const query = `
+		INSERT INTO usuario (nombre, correo, contrasena)
+		VALUES ($1, $2, $3)
+		RETURNING *;
+	`;
+	try {
+		await client.connect();
+		const res = await client.query(query, [nombre, correo, contrasena]);
+		console.log('Usuario creado:', res.rows[0]);
+		return res.rows[0];
+	} catch (err) {
+		console.error('Error creando usuario:', err);
+		throw err;
+	} finally {
+		await client.end();
+	}
+}
 
 // Función para crear la tabla usuario si no existe
 export async function crearTablaUsuario() {
