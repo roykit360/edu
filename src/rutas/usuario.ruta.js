c340b78c6c7cefc7559a5ddd251714b8e6160987
roykit360 <rroy87872@gmail.com>
2025-08-29 11:42:53 -0500

    Update: usuario.rutas.js

--- src/rutas/usuario.ruta.js
+++ src/rutas/usuario.ruta.js
@@ -1,53 +1,18 @@
-//import { crearTablaUsuario } from '../modelo/usuario.modelo.js';
-import{ login, register, logout } from "../controllers/auth.controllers.js"
-import { obtenerAdmins } from '../modelo/admin.modelo.js';
-import { obtenerAdmins } from '../rutas/usuario.ruta.js';
 import { Router } from "express";
+import {
+  login,
+  logout,
+  register,
+  verifyToken,
+} from "../controllers/auth.controller.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
 
 const router = Router();
 
-router.get('/admins/console', async (req, res) => {
-    try {
-        const admins = await obtenerAdmins();
-        console.log('Datos de admin:', admins);
-        res.status(200).json(admins);
-    } catch (err) {
-        console.error('Error consultando admin:', err);
-        res.status(500).json({ message: 'Error consultando admin', error: err.message });
-    }
-});
-router.get('/usuarios', (req, res) => {
-    res.send({ message: 'Lista de usuarios' });
-});
+router.post("/register", validateSchema(registerSchema), register);
+router.post("/login", validateSchema(loginSchema), login);
+router.get("/verify", verifyToken);
+router.post("/logout", verifyToken, logout);
 
-router.get('/usuarios/:id', (req, res) => {
-    const { id } = req.params;
-    res.send({ message: `Detalles del usuario con ID: ${id}` });
-});
-
-router.post('/usuarios', (req, res) => {
-    const nuevoUsuario = req.body;
-    res.send({ message: 'Usuario creado', usuario: nuevoUsuario });
-});
-
-router.delete('/usuarios/:id', (req, res) => {
-    const { id } = req.params;
-    res.send({ message: `Usuario con ID: ${id} eliminado` });
-});
-
-router.put('/usuarios/:id', (req, res) => {
-    const { id } = req.params;
-    const usuarioActualizado = req.body;
-    res.send({ message: `Usuario con ID: ${id} actualizado`, usuario: usuarioActualizado });
-});
-
-router.patch('/usuarios/:id', (req, res) => {
-    const { id } = req.params;
-    const cambios = req.body;
-    res.send({ message: `Usuario con ID: ${id} parcialmente actualizado`, cambios });
-});
-
-router.options('/usuarios', (req, res) => {
-    res.set('Allow', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    res.sendStatus(200);
-});
+export default router;
