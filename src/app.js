27a09a754366ee78c41d47b0b680f4d855cfe1d0
roykit360 <rroy87872@gmail.com>
2025-08-29 11:04:05 -0500

    update: jwt.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 
 import usuarioRoutes from "./rutas/usuario.ruta.js";
+
 const app = express();
 
 app.use(morgan('dev'));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import app from './app.js' 
+import auth from './rutas/auth.ruta.js'; 
 
+app.use('/auth', auth);
 
 app.listen(4000)
 console.log('Server is running on port', 4000)
